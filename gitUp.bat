d59d6cb159dfd0e8a146301e7ca432458214c17e
eu <en.gustavo@gmail.com>
2022-11-02 17:17:53 -0300

    v0.0.1 #1 - Shortcuts, cleaning utilities, notes

---
+++ gitUp.bat
@@ -0,0 +1,22 @@
+echo off
+
+set major=0
+set minor=0
+set sub=1
+set build=1
+
+git status
+pause
+
+git add *
+git status
+pause
+
+echo Enter commit message
+set /P "Message="
+
+git commit -m "v%major%.%minor%.%sub% #%build% - %Message%"
+
+git push https://github.com/dilara255/AgentSystem.git
+
+pause
