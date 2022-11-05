0d4fd260b22e5d62db8907219cbfe9abb21d89a6
eu <en.gustavo@gmail.com>
2022-11-05 01:53:46 -0300

    v0.1.0 #1 - Agent Data Structure Allocation

---
+++ AgentSystem/src/agentData.cpp
@@ -0,0 +1,67 @@
+#include "miscStdHeaders.h"
+#include "core.hpp"
+
+#include "agentDataStructure.hpp"
+#include "AS_internal.hpp"
+
+void allocateMemoryForAgentSystems(int numberLAs, int numberGAs, int neighborMaxLAs) {
+	//Allocates the entire thing and sets pointers.
+	//See agentDataStructure.hpp for the layout.
+	
+	AS::agentControlSystem_ptr.numberOfLAs = numberLAs;
+	AS::agentControlSystem_ptr.numberOfGAs = numberGAs;
+	AS::agentControlSystem_ptr.neighborMaxLAs = neighborMaxLAs;
+
+	AS::agentControlSystem_ptr.lengthLAonOffList = 
+		(int)ceil((float)numberLAs/(sizeof(AS::bitfield_t)*8));
+	AS::agentControlSystem_ptr.lengthGAonOffList = 
+		(int)ceil((float)numberGAs/(sizeof(AS::bitfield_t)*8));
+
+	int lengthLAonOffList = AS::agentControlSystem_ptr.lengthLAonOffList;
+	int lengthGAonOffList = AS::agentControlSystem_ptr.lengthGAonOffList;
+	int neighborRelationBytes = 2*sizeof(float) + sizeof(int);
+	int LAneighborRelationDataSize = neighborRelationBytes * neighborMaxLAs * numberLAs;
+	int GAconnectionsLenght = lengthGAonOffList;
+
+	AS::agentControlSystem_ptr.LAsystemsPtrs.nameList_ptr =
+		(AS::agentName_t*)malloc(sizeof(AS::agentName_t) * numberLAs);
+	AS::agentControlSystem_ptr.LAsystemsPtrs.onOffList_ptr =
+		(AS::bitfield_t*)malloc(sizeof(AS::bitfield_t) * lengthLAonOffList);
+	AS::agentControlSystem_ptr.LAsystemsPtrs.parameterList_ptr =
+		(AS::parametersLA_t*)malloc(sizeof(AS::parametersLA_t) * numberLAs);
+	//FIX: this is kinda hacky:
+	AS::agentControlSystem_ptr.LAsystemsPtrs.locationAndConnectionData_ptr =
+		(AS::locationAndConnectionDataLA_t*)malloc(
+			(sizeof(AS::locationAndConnectionDataLA_t)+(numberLAs -1)*sizeof(AS::bitfield_t))
+				* numberLAs);
+	AS::agentControlSystem_ptr.LAsystemsPtrs.neighborRelationsList_ptr =
+		(AS::neighborRelationPtrs_t*)malloc(LAneighborRelationDataSize);
+	AS::agentControlSystem_ptr.LAsystemsPtrs.tresholdsAndInfluenceData_ptr =
+		(AS::thresholdsAndGAinfluenceOnLA_t*)malloc(
+			sizeof(AS::thresholdsAndGAinfluenceOnLA_t) * numberLAs);
+
+	AS::agentControlSystem_ptr.GAsystemsPtrs.nameList_ptr =
+		(AS::agentName_t*)malloc(sizeof(AS::agentName_t) * numberGAs);
+	AS::agentControlSystem_ptr.GAsystemsPtrs.onOffList_ptr =
+		(AS::bitfield_t*)malloc(sizeof(AS::bitfield_t) * lengthGAonOffList);
+	//FIX: this is kinda hacky:
+	AS::agentControlSystem_ptr.GAsystemsPtrs.LAparameterList_ptr =
+		(AS::associatedLAparameters_t*)malloc(
+			(sizeof(AS::associatedLAparameters_t)+(numberLAs-1)*sizeof(AS::bitfield_t))
+				*numberGAs);
+	//FIX: this is kinda hacky:
+	AS::agentControlSystem_ptr.GAsystemsPtrs.GAconnectionsList_ptr =
+		(AS::bitfield_t*)malloc(sizeof(AS::bitfield_t)*GAconnectionsLenght*numberGAs);
+	AS::agentControlSystem_ptr.GAsystemsPtrs.neighborRelationsList_ptr =
+		(AS::neighborRelationPtrs_t*)malloc(neighborRelationBytes*numberGAs);
+	AS::agentControlSystem_ptr.GAsystemsPtrs.personalitiesList_ptr =
+		(AS::GApersonality*)malloc(
+			sizeof(AS::GApersonality) * numberGAs);
+
+	//TODO: set internal pointers 
+	//actually, re-think pointers inside "internal" structs
+	//TODO: tie this to some initialization function and also set and verify some values
+}
+
+
+
--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # AgentSystem
 
-Using premake (included) to create VS 2019 solution.
-Uses spdlog for logging.
+Using premake (included) to create VS 2019 solution.<br />
+Uses spdlog for logging.<br />
 Aux0 is little a helper project of mine (logging, dlls and precompiled headers).
 
 Progress overview (may be a few days old):
 
-v0.1 -Project scafolding (done);
-v0.2 -Basic data structures, loading and exporting (~35%);
-v0.4 -Runtime loop and realtime data manipulation;
-v0.5 -Some tooling;
-v0.6 -Groundwork for decision making and a couple actions + decisions;
+v0.1 -Project scafolding (done);<br />
+v0.2 -Basic data structures, loading and exporting (~50%);<br />
+v0.4 -Runtime loop and realtime data manipulation;<br />
+v0.5 -Some tooling;<br />
+v0.6 -Groundwork for decision making and a couple actions + decisions;<br />
 v1.0 -Actual behaviours, targeted prototype and final testing.
 
 More detaild version planning and info on AgentSystem/src/notes.hpp
