f64a17c3f275a183e2556c47927e18565d50649f
eu <en.gustavo@gmail.com>
2023-02-17 19:16:02 -0300

    v0.3.2 #18 - Reorganization to implement tests of sleeping and prng

--- Aux0/APIs/timeHelpers.hpp
+++ Aux0/APIs/timeHelpers.hpp
@@ -10,7 +10,7 @@ namespace AZ{
 
 	const static std::chrono::microseconds standardThreshold(AZ_MICROS_TO_BUSY_WAIT);
 
-	//takes just about 0.22 microseconds on test system
+	//takes about 0.22 microseconds per call on test system
 	inline std::chrono::microseconds nowMicros() {
 		auto now = std::chrono::steady_clock::now().time_since_epoch();
 		return std::chrono::duration_cast<std::chrono::microseconds>(now);
@@ -22,30 +22,16 @@ namespace AZ{
 	}
 
 	//This loops while yielding via Sleep(0) up to targetWakeTime - threshold,
-	//then busy-waits until threshold. Does nothing if now >= targetWakeTime.
+	//then busy-waits until targetWakeTime. Does nothing if now >= targetWakeTime.
 	//If no threshold is especified, uses standard value defined by Aux0.
-	//WARNING: NO IDEA how portable this is (HACKY?)
-	//TODO-CRITICAL: Implement testing of this specifically
-	static void hybridBusySleep(std::chrono::steady_clock::time_point targetWakeTime,
-		                     std::chrono::microseconds threshold = standardThreshold) {
-
-		int dummy = 0;
-		static std::chrono::microseconds zeroMicro(0);
-
-		while ((targetWakeTime - std::chrono::steady_clock::now()) > threshold) {
-			std::this_thread::sleep_for(zeroMicro);
-		}
-		while (std::chrono::steady_clock::now() < targetWakeTime){dummy++;}
-	}
-
+	//WARNING: NO IDEA how portable this is (is it HACKY?)
+	//TODO: test response for different load scenarios
+	//TODO: run tests on other platforms
+	void hybridBusySleep(std::chrono::steady_clock::time_point targetWakeTime,
+		                 std::chrono::microseconds threshold = standardThreshold);
 	//This loops while yielding via Sleep(0) for microsToSleep - threshold,
 	//then busy-waits to complete microsToSleep. 
 	//If no threshold is especified, uses standard value defined by Aux0.
-	static void hybridBusySleepForMicros(std::chrono::microseconds microsToSleep,
-		                 std::chrono::microseconds threshold = standardThreshold){
-
-		auto targetWakeTime = std::chrono::steady_clock::now() + microsToSleep;
-
-		hybridBusySleep(targetWakeTime, threshold);
-	}
+	void hybridBusySleepForMicros(std::chrono::microseconds microsToSleep,
+		                          std::chrono::microseconds threshold = standardThreshold);
 }
---
+++ Aux0/src/tests.cpp
@@ -0,0 +1,22 @@
+#include "tests.hpp"
+#include "miscStdHeaders.h"
+
+namespace AZ{
+
+    bool testDraw4spcg32s(int howManyToDraw) {
+
+        return false;
+    }
+
+	bool testDraw1spcg32(int howManyToDraw) {
+
+        return false;
+    }
+
+	double testHybridBusySleeping(int minimumSleepTimeMicros, int maximumSleepTimeMicros, 
+			                double sleepTimeMultiplierPerStep, int howManyThresholdSteps,
+			                       int howManyParallelLoadSteps, double margin, bool log) {
+
+		return 0;
+	}
+}
